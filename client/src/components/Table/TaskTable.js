b9b626e5b590e433597094bb76c5ad83367afcca
Daniel <daniel@spacelab.com.br>
2018-04-06 19:50:23 -0300

    Update

--- client/src/components/Table/TaskTable.js
+++ client/src/components/Table/TaskTable.js
@@ -18,9 +18,8 @@ class TaskTable extends Component {
 		 */
 		this.state = {
 			loading: true,
-			orderBy: 'dataFinal',
+			sortBy: 'id',
 			filterBy: [],
-			filterValue: '',
 			tasks: []
 		}
 
@@ -41,15 +40,87 @@ class TaskTable extends Component {
 	}
 
 
-	filterBy(column){
+	addFilter(column){
 
 		this.setState((prevState) => {
 
-			prevState.filterBy.push(column);
+			let $field = document.querySelector(`#filter_${column}`);
+
+			// checks the value of the filter
+			if ( $field.value !== "" ) {
+
+				// adds the column name to the array of filtered columns if isn't there
+				if ( prevState.filterBy.findIndex(x => x === column) < 0 ) {
+					prevState.filterBy.push(column);
+				}
+
+			// if the are no filter, removes the column of the array
+			} else {
+
+				let index = prevState.filterBy.indexOf(column);
+
+				if ( index > -1 ) {
+					prevState.filterBy.splice(index, 1);
+				}
+
+			}
 
 			return {
 				filterBy: prevState.filterBy
 			}
+		});
+
+	}
+
+
+	filterTasks(tasks){
+
+		const filters = this.state.filterBy;
+
+		if ( filters.length === 0 ) return tasks;
+
+		function filterByColumn(task){
+
+			// must become true for the task to be removed from the list
+			let filtered = false;
+
+			filters.forEach(filter => {
+
+				let $field = document.querySelector(`#filter_${filter}`);
+
+				if ( task[filter] ) {
+					if ( task[filter].toString().toLowerCase().indexOf( $field.value.toLowerCase() ) !== -1 ) {
+						filtered = true;
+					}
+				}
+
+			});
+
+			return filtered;
+
+		}
+
+		return tasks.filter(filterByColumn);
+
+	}
+
+
+	sortTasks(tasks, propSort = this.state.sortBy, asc = true){
+
+		return tasks.sort((a, b) => {
+			if ( a[propSort] === null ) {
+				return 1;
+			}
+			if ( b[propSort] === null ) {
+				return -1;
+			}
+			if ( a[propSort] < b[propSort] ){
+				return -1;
+			}
+			if ( a[propSort] > b[propSort] ){
+				return 1;
+			}
+			return 0;
 		});
 
 	}
@@ -65,52 +136,60 @@ class TaskTable extends Component {
 			return <div>Loading tasks...</div>
 		}
 
+		const filteredTasks = this.sortTasks(this.filterTasks(tasks));
+
 		if ( tasks.length ) {
 			return (
 					<div>
 					{(() => {
 						if ( filterBy.length ) {
-							return <p>Filtrando por: { filterBy.join(',') }</p>;
+							return <p>Filtrando por: { filterBy.join(', ') }</p>;
 						}
 					})()}
 					<table>
 						<thead>
 							<tr>
-								<th>
-									OS
+								<th onClick={ () => { this.setState({sortBy: 'id'})} }>
+									OS<br/>
+									<input type="text" id="filter_id" placeholder="Filter by OS" onChange={ () => this.addFilter('id') } />
 								</th>
-								<th>
-									Task
-									<input type="text" id="filter_task" placeholder="Filter by Task Name" onChange={ () => this.filterBy('task') } />
+								<th onClick={ () => { this.setState({sortBy: 'task'})} }>
+									Task<br/>
+									<input type="text" id="filter_task" placeholder="Filter by Task Name" onChange={ () => this.addFilter('task') } />
 								</th>
-								<th>
-									Client
+								<th onClick={ () => { this.setState({sortBy: 'clientName'})} }>
+									Client<br/>
+									<input type="text" id="filter_clientName" placeholder="Filter by Client Name" onChange={ () => this.addFilter('clientName') } />
 								</th>
-								<th>
-									Project
+								<th onClick={ () => { this.setState({sortBy: 'projectName'})} }>
+									Project<br/>
+									<input type="text" id="filter_projectName" placeholder="Filter by Project Name" onChange={ () => this.addFilter('projectName') } />
 								</th>
-								<th>
-									Done by
+								<th onClick={ () => { this.setState({sortBy: 'userName'})} }>
+									Done by<br/>
+									<input type="text" id="filter_userName" placeholder="Filter by User Name" onChange={ () => this.addFilter('userName') } />
 								</th>
-								<th title="DD/MM/YYYY">
-									Start Date
+								<th title="DD/MM/YYYY" onClick={ () => { this.setState({sortBy: 'dataInicio'})} }>
+									Start Date<br/>
+									<input type="text" id="filter_dataInicio" placeholder="Filter by Start Date" onChange={ () => this.addFilter('dataInicio') } />
 								</th>
-								<th>
-									Conclusion Date
+								<th onClick={ () => { this.setState({sortBy: 'dataFim'})} }>
+									Conclusion Date<br/>
+									<input type="text" id="filter_dataFim" placeholder="Filter by Conclusion Date" onChange={ () => this.addFilter('dataFim') } />
 								</th>
 							</tr>
 						</thead>
 						<tbody>
 							{
-								tasks.map(task =>
+								filteredTasks.map(task =>
 									<tr key={ task.id }>
-										<td>{ task.id }</td>
+										<td align="center">{ task.id }</td>
 										<td>{ task.task }</td>
-										<td>{ task.clientName }</td>
-										<td>{ task.projectName }</td>
-										<td>{ task.userName }</td>
-										<td>{ getFormattedDate(task.dataInicio) }</td>
-										<td>{ getFormattedDate(task.dataFim) }</td>
+										<td align="center">{ task.clientName }</td>
+										<td align="center">{ task.projectName }</td>
+										<td align="center">{ task.userName }</td>
+										<td align="center">{ getFormattedDate(task.dataInicio) }</td>
+										<td align="center">{ getFormattedDate(task.dataFim) }</td>
 									</tr>
 								)
 							}
